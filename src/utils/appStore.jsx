49bca3d96b5ad83f6d472cd1fcd359a058c4d66f
sourabh-bhatt <sourabhbhatt825@gmail.com>
2023-12-30 18:56:50 +0530

    user sign up successful

--- src/utils/appStore.jsx
+++ src/utils/appStore.jsx
@@ -1,12 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { userReducer } from "./userSlice";
+import userReducer  from "./userSlice";
 
-const appStore = configureStore{
-    {
+const appStore = configureStore({
     reducer: {
         user: userReducer
     }
-}
-}
+
+});
 
 export default appStore;
