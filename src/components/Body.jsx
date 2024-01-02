d2e312f02ab6a0dd4d60cad7b8c008e0774028b2
sourabh-bhatt <sourabhbhatt825@gmail.com>
2024-01-03 02:00:10 +0530

    Code formatted

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,16 +1,9 @@
-// import { useEffect } from "react";
-// import { useDispatch } from "react-redux";
-// import { onAuthStateChanged } from "firebase/auth";
-// import { auth } from "../utils/firebase";
-// import { addUser, removeUser } from "../utils/userSlice";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Error from "./Error";
 import Browse from "./Browse";
 import Login from "./Login";
 
 const Body = () => {
-  // const dispatch = useDispatch();
-
   const appRouter = createBrowserRouter([
     {
       path: '/',
@@ -26,30 +19,8 @@ const Body = () => {
     }
   ]);
 
-  // Sign in state change
-  // useEffect(() => {
-  //   onAuthStateChanged(auth, (user) => {
-  //     if (user) {
-  //       const { uid, email, displayName, photoURL } = user;
-  //       dispatch(
-  //         addUser({
-  //           uid: uid,
-  //           email: email,
-  //           displayName: displayName,
-  //           photoURL: photoURL,
-  //         })
-  //       );
-  //     } else {
-  //       dispatch(
-  //         removeUser());
-  //     }
-  //   });
-  // }, []);
-
   return (
-    <>
-      <RouterProvider router={appRouter} />
-    </>
+    <RouterProvider router={appRouter} />
   );
 };
 
