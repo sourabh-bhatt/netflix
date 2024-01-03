386170c9dab8b04059bb2085b5f73848d5e91973
sourabh-bhatt <sourabhbhatt825@gmail.com>
2024-01-03 23:43:17 +0530

    Added background video on loop

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -5,14 +5,14 @@ import { faPlay, faCircleInfo } from '@fortawesome/free-solid-svg-icons';
 const VideoTitle = ({title, overview}) => {
   return (
     <>
-        <div className="pt-16 px-12">
+        <div className="w-screen aspect-video pt-[20%] px-24 absolute text-white bg-gradient-to-r from-black">
             <h1 className="text-5xl font-bold w-1/2">{title}</h1>
             <p className="py-6 text-lg w-1/3">{overview}</p>
             <div className="">
-                <button className=" bg-black border-2 border-black text-black p-4 px-12 rounded-lg bg-opacity-50 ">
+                <button className=" bg-white hover:bg-opacity-80 border-2  text-black p-4 px-12 rounded-lg  ">
                 <FontAwesomeIcon icon={faPlay} /> Play
                 </button>
-                <button className=' mx-2 bg-black border-2 border-black text-black p-4 px-10 rounded-lg bg-opacity-50'>
+                <button className=' mx-2 bg-gray-400 hover:bg-opacity-80 text-black p-4 px-10 rounded-lg bg-opacity-50'>
                 <FontAwesomeIcon icon={faCircleInfo} /> More Info
                 </button>
             </div>
