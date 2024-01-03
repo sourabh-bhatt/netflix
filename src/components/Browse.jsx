008c581f41db0e609ed1d403de016709c25e6bd3
sourabh-bhatt <sourabhbhatt825@gmail.com>
2024-01-03 17:06:19 +0530

    Created movie reducer and slice

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -10,7 +10,7 @@ const Browse = () => {
     const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
 
     const json = await data.json();
-    console.log(json);  
+    console.log(json.results);  
   }
 
   useEffect(()=>{
